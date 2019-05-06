0e80cba31df5bd9b9a25a7b71140256a31d7c79f
Kevin Stange <kevin@steadfast.net>
2019-05-06 12:02:32 -0500

    Support for GNOME 3.32.  All older GNOME support removed.

--- extension.js
+++ extension.js
@@ -11,60 +11,53 @@ const Convenience = Me.imports.convenience;
 const Gettext = imports.gettext.domain('gnome-shell-open-terminal');
 const _ = Gettext.gettext;
 
-function init() {
-    Convenience.initTranslations("gnome-shell-open-terminal");
+function init(extensionMeta) {
+    return new OpenTerminalExtension(extensionMeta);
 }
 
-function reloadLayout() {
-    LayoutManager._bgManagers.forEach(
-        function(mgr) {
-            if (!mgr.background)
-                mgr.emit('changed');
-            else // < gnome-shell-3.12?
-                mgr.background.emit('changed');
-        });
-}
+class OpenTerminalExtension {
+    constructor(extensionMeta) {
+        Convenience.initTranslations("gnome-shell-open-terminal");
+    }
 
-function enable() {
-    // patch PopupMenu
-    PopupMenu.PopupMenu.prototype.addActionAt = function(title, position, callback) {
-        let menuItem = new PopupMenu.PopupMenuItem(title);
-        this.addMenuItem(menuItem, position);
-        menuItem.connect('activate', Lang.bind(this, function(menuItem, event) {
-            callback(event);
-        }));
-        return menuItem;
-    };
-    PopupMenu.PopupMenu.prototype.addAppAction = function(title, desktopFile, position) {
-        let menuItem = this.addActionAt(title, position, function(event) {
-            let app = Shell.AppSystem.get_default().lookup_app(desktopFile);
-            if (!app) {
-                log('Application for desktop file ' + desktopFile + ' could not be found!');
-                return;
-            }
-            Main.overview.hide();
-            app.open_new_window(-1);
-        });
-        menuItem.actor.visible = true;
-        return menuItem;
-    };
+    enable() {
+        PopupMenu.PopupMenu.prototype.addActionAt = function(title, position, callback) {
+            let menuItem = new PopupMenu.PopupMenuItem(title);
+            this.addMenuItem(menuItem, position);
+            menuItem.connect('activate', Lang.bind(this, function(menuItem, event) {
+                callback(event);
+            }));
+            return menuItem;
+        };
 
-    // patch BackgroundMenu
-    BackgroundMenu.BackgroundMenu.prototype._old_init = BackgroundMenu.BackgroundMenu.prototype._init;
-    BackgroundMenu.BackgroundMenu.prototype._init = function(source) {
-        this._old_init(source);
-        this.actor.add_style_class_name('popup-menu-box');
-        this.addAppAction(_("Open Terminal"), 'gnome-terminal.desktop', 0);
-    };
+        PopupMenu.PopupMenu.prototype.addAppAction = function(title, desktopFile, position) {
+            let menuItem = this.addActionAt(title, position, function(event) {
+                let app = Shell.AppSystem.get_default().lookup_app(desktopFile);
+                if (!app) {
+                    log('Application for desktop file ' + desktopFile + ' could not be found!');
+                    return;
+                }
+                Main.overview.hide();
+                app.open_new_window(-1);
+            });
+            menuItem.actor.visible = true;
+            return menuItem;
+        };
 
-    reloadLayout();
-}
+        LayoutManager._bgManagers.forEach(
+            function(mgr) {
+                let actor = mgr.backgroundActor;
+                actor._backgroundMenu.addAppAction(_("Open Terminal"), 'org.gnome.Terminal.desktop', 0);
+                actor._backgroundManager.addMenu(actor._backgroundMenu);
+            });
+    }
 
-function disable() {
-    BackgroundMenu.BackgroundMenu.prototype._init = BackgroundMenu.BackgroundMenu.prototype._old_init;
-    BackgroundMenu.BackgroundMenu.prototype._old_init = undefined;
-    PopupMenu.PopupMenu.prototype.addAppAction = undefined;
-    PopupMenu.PopupMenu.prototype.addActionAt = undefined;
-
-    reloadLayout();
+    disable() {
+        LayoutManager._bgManagers.forEach(
+            function(mgr) {
+                let actor = mgr.backgroundActor;
+                actor._backgroundMenu.destroy();
+                BackgroundMenu.addBackgroundMenu(actor, LayoutManager);
+            });
+    }
 }
